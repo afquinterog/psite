6350792948c2f54be8b51643315096cb355292a2
Andres Quintero <afquinterog@users.noreply.github.com>
2023-04-27 22:17:17 -0500

    update contact message

--- message.php
+++ message.php
@@ -1,12 +1,12 @@
 <?php
-$to      = 'andresquintero@zeroplatform.com';
+$to      = 'rocoutp@gmail.com';
 $subject = 'Job Contact';
 $message = $_POST["email"] . " / " . $_POST["message"];
-$headers = 'From: afquintero.tk' . "\r\n" .
-    'Reply-To: andresquintero@zeroplatform.com' . "\r\n" .
+$headers = 'From: afq.servicesapp.co' . "\r\n" .
+    'Reply-To: rocoutp@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
 
-header("Location: http://afquintero.tk/index.php?msg=1#contact");
+header("Location: http://afq.servicesapp.co/index.php?msg=1#contact");
 die();
